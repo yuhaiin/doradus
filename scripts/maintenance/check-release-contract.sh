96b749b98862afc2504d5eec5791395006c575e1
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2026-08-14 11:12:02 +0800

    ci: guard Windows cross cache configuration

--- scripts/maintenance/check-release-contract.sh
+++ scripts/maintenance/check-release-contract.sh
@@ -85,6 +85,9 @@ done
 required_windows_cross_literals=(
   'cargo_home="${YUHAIIN_RELEASE_WINDOWS_CARGO_HOME:-${cache_root}/release-windows-cargo-home}"'
   '-v "${cargo_home}:/cargo-home:Z"'
+  'export CARGO_HOME=/cargo-home'
+  'export CARGO_TARGET_DIR=/target'
+  'export TMPDIR=/state/cache/tmp'
   'unset CARGO_NET_OFFLINE'
   'cargo check --config net.offline=false --locked --target'
 )
